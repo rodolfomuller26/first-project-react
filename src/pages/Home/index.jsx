095e946a428947bddb7c18a08aa6b3feeea048d4
rodolfomuller26 <rodolfomuller@msn.com>
2024-08-21 21:28:44 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,75 @@
+import { useRef } from 'react'
+import api from '../../services/api'
+
+import { Title,
+         Container,
+         TopBackground,
+         Form,
+         ContainerInputs,
+         Input,
+         InputLabel,
+         Button }
+from './styles'
+
+import UsersImage from '../../assets/users.png'
+
+function Home() {
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  async function registerNewUser() {
+    await api.post('/usuarios', {
+      email: inputEmail.current.value,
+      age: parseInt(inputAge.current.value),
+      name: inputName.current.value
+    })
+}
+
+  return (
+
+      <Container>
+
+          <TopBackground>
+            <img src={UsersImage} alt='imagem-usuarios'/>
+          </TopBackground>
+
+      <Form>
+
+          <Title> Cadastrar Usuário </Title>
+
+          <ContainerInputs>
+
+              <div>
+                <InputLabel>
+                  Nome <span> * </span>
+                </InputLabel>
+                <Input type='text' placeholder='Nome do Usuário' ref={inputName}/>
+              </div>
+
+              <div>
+                <InputLabel>
+                  Idade <span> * </span>
+                </InputLabel>
+                <Input type='number' placeholder='Idade do Usuário' ref={inputAge}/>
+                </div>
+
+        </ContainerInputs>
+
+            <div style={{ width: '100%' }}>
+              <InputLabel>
+                  E-mail <span> * </span>
+              </InputLabel>
+              <Input type='email' placeholder='E-mail do Usuário' ref={inputEmail}/>
+            </div>
+
+        <Button type='button' onClick={registerNewUser}> Cadastrar Usuário </Button>
+
+      </Form>
+
+      </Container>   
+  )
+}
+
+export default Home
